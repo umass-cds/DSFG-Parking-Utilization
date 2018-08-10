15f531b3551d19b7ce8110f3aa4f6ae680e83606
katiehouse3 <katiehouse3@gmail.com>
2018-08-10 16:34:32 -0400

    add machine learning preprocessing

--- preprocessing/parkmobile-data/parkmobile_occupancy.py
+++ preprocessing/parkmobile-data/parkmobile_occupancy.py
@@ -11,7 +11,7 @@ import glob
 import xlrd
 import csv
 import os
-
+import datetime as dt
 # ~~~ FUNCTIONS ~~~
 def csv_from_excel(xls_file_name, first_run):
 	# Open the *.xls file
@@ -32,7 +32,6 @@ def csv_from_excel(xls_file_name, first_run):
 # ~~~ MAIN FUNCTION ~~~
 def main(): 
 	path = "csvs/*.xls"
-
 	try:
 	    os.remove('output.csv')
 	except:
@@ -47,17 +46,32 @@ def main():
 
 	# Format Dataframe
 	pm_df = pd.read_csv('output.csv', encoding='utf-8', sep=',', index_col=0)
-	df['time'] = df['time'].astype('datetime64[ns]')
-
 	pm_df.reset_index(drop = True, inplace = True)
-	pm_df['parkingMinutes'] = pm_df['Parking Amount'] * 60
+	pm_df['datetime'] = pd.TimedeltaIndex(pm_df['Insert Date'], unit='d') + dt.datetime(1899, 12, 30)
+	pm_df['start_time'] = pd.to_datetime(pm_df['datetime'])
+	pm_df['parking_mins'] = pm_df['Parking Amount'] * 60
+	pm_df['minute_delta'] = pd.to_timedelta(pm_df['parking_mins'], unit='m')
+	pm_df['end_time'] = pm_df['start_time'] + pm_df['minute_delta'] 
 	print(pm_df)
+	
+	# Initialize new Dataframe
+	d = []
 	for index, row in pm_df.iterrows():
-		minute_window = row['parkingMinutes'] / 15
+		minute_window = int((row['parking_mins'] / 15) + 15)
+		minutes = 0 # Initialize Minutes
 		for i in range(minute_window):
-			time_step = pm_d['Insert Date'] + minute_window
-			pm_output.append(time_step)
+			occ_time = row['start_time']+ pd.Timedelta(minutes=minutes)
+			d.append({'occupancy': occ_time})
+			minutes += 15
 
-
+	df =  pd.DataFrame(data=d)
+	agg_by_15 = df.groupby([df.occupancy.dt.month,df.occupancy.dt.day,\
+							df.occupancy.dt.hour,\
+							round(df.occupancy.dt.minute/15,0)]).count()
+	agg_by_15.index.names = ['month','day','hour','min']
+	agg_by_15 = agg_by_15.reset_index()
+	agg_by_15.to_csv('occupancy.csv', index=False)
+	
+	
 if __name__ == "__main__":
     main()
---
+++ preprocessing/townhall-data/preprocess-ml-data.py
@@ -0,0 +1,47 @@
+from os import listdir
+from os.path import isfile, join
+import numpy
+import cv2
+import sys
+
+# DELETE .DS-STORE in dataset folder if issues with first file in array
+
+mypath = 'C:\\Users\\house\\Google Drive (khouse@umass.edu)\\DS4Good - Parking Area Utilization\\Datasets\\Town Hall Lot Dataset\\pictures (1)'
+img_path = []
+
+# Store all images in an Array
+print("Collecting all Images in Path...")
+onlyfiles = [ f for f in sorted(listdir(mypath)) if isfile(join(mypath,f)) ]
+images = numpy.empty(len(onlyfiles), dtype=object)
+for n in range(0, len(onlyfiles)):
+	try:
+		images[n] = cv2.imread(join(mypath,onlyfiles[n]) )
+		img_path += onlyfiles
+	except:
+		print('hello')
+
+# Loop through each image and crop/save
+print("\nProcessing all Images in Path...")
+count = 0
+for image in images[:-1]:
+    image_name = img_path[count][:-4]
+    print("Processing %s Image" % image_name)
+    crop = [image[88:110, 480:502] , image[101:123, 476:498],image[113:135, 469:491],\
+		image[122:144, 460:482], image[137:164, 445:472],image[155:182, 451:478], \
+		image[170:197, 436:463], image[183:218, 414:449], image[208:243, 399:434],\
+		image[230:271, 381:422], image[255:296, 375:416], image[287:328, 357:398],\
+		image[311:359, 328:376], image[357:412, 300:355], image[399:454, 276:331], \
+		image[445:503, 245:303], image[503:567, 219:283], image[70:102, 709:741],\
+		image[93:117, 729:753], image[103:127, 732:756], image[119:143, 734:758], \
+		image[143:167, 736:760], image[158:182, 743:767], image[171:204, 745:778], \
+		image[189:222, 759:792], image[214:258, 771:815], image[251:301, 776:826], \
+		image[277:332, 799:854], image[317:372, 815:870], image[355:410, 828:883], \
+		image[412:467, 845:900]]
+    for j in range(len(crop)):
+    	p = crop[j]
+    	cv2.imwrite("cropped-images/" + image_name + "-p" + str(j) + ".jpg", p)
+    count += 1
+
+print("Processing Complete!")
+
+cv2.destroyAllWindows()
